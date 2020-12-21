406a491a6b27013fb2dba41d50f690ca88dc1d3b
Kevin Ngo <me@ngokevin.com>
2020-12-21 15:24:12 -0800

    logging off

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(null, null, null, {
   dialect: 'sqlite',
+  logging: false,
   storage: path.join(__dirname, './db/cc-cedict.sqlite')
 });
 
@@ -13,12 +14,12 @@ var Word = sequelize.define('Word', {
   definitions: Sequelize.STRING
 });
 
-// convert between traditional and simplified
+// Convert between traditional and simplified.
 var cnchars = require('cn-chars');
-// prettify the pinyin default (letters + numbers) in CC-CEDICT
+// Prettify the pinyin default (letters + numbers) in CC-CEDICT.
 var pinyin = require('prettify-pinyin');
 
-module.exports.searchByChinese = function(str, cb){
+module.exports.searchByChinese = function (str, cb) {
   var simplified = str.slice().split('');
   var traditional = str.slice().split('');
   for (var i = 0; i < str.length; i++){
@@ -55,13 +56,12 @@ module.exports.searchByChinese = function(str, cb){
 };
 
 module.exports.searchByPinyin = function(str, cb) {
-	
 	// Catches dead-tones or 5th tone
 	var parts = str.split(" ");
 	var newStr = [];
 	_.each(parts, function(part) {
 		var numeric = part.replace(/\D/g,'');
-		
+
 		if (numeric === "") {
 			part += "5";
 			newStr.push(part);
@@ -69,18 +69,18 @@ module.exports.searchByPinyin = function(str, cb) {
 			newStr.push(part);
 		}
 	});
-	
+
 	str = "[" + newStr.join(" ") + "]";
-	
+
 	var query = {
 		where: {pronunciation: str}
 	};
-	
+
 	Word
 		.findAll(query)
 		.then(function(words) {
 			var results = [];
-			
+
 			_.each(words, function(word) {
 				var pronunciation = word.pronunciation;
 			        var prettified = pinyin.prettify(pronunciation.slice(1, pronunciation.length - 1));
@@ -95,6 +95,6 @@ module.exports.searchByPinyin = function(str, cb) {
 	});
 }
 
-module.exports.searchByEnglish = function(str, cb){
+module.exports.searchByEnglish = function(str, cb) {
   // TODO
 };
